6cd9eda8eb93ca89f278dcd524d25f2309ec6143
lefer1207 <lefer1207@gmail.com>
2026-06-04 18:18:24 -0300

    Actualizar SW a v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'registro-policial-v4';
+const CACHE_NAME = 'registro-policial-v5';
 const ASSETS = ['./index.html', './offline_test.html', './'];
 self.addEventListener('install', (e) => { e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))); });
